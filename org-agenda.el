2b1bc2ab00e59c68e61cd39bed0ced18bbd1847d
Peter de Jong <p.de.jong@gmail.com>
2011-07-26 22:40:32 +0200

    agenda deadline announce fix

--- org-agenda.el
+++ org-agenda.el
@@ -3,6 +3,12 @@
 (setq org-agenda-include-diary t)
 ;; Restore window on quit agenda
 (setq org-agenda-restore-windows-after-quit t)
+;; Don't show deadline if done
+(org-agenda-skip-deadline-if-done t)
+;; Don't show scheduled if done
+(org-agenda-skip-scheduled-if-done t)
+;; Announce deadlines 7 days ahead
+(org-deadline-warning-days 7)
 ;; Set files for agenda views
 (setq org-agenda-files (list gtd-file-nextactions
 			     gtd-file-projects
