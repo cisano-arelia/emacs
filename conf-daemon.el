43db5a12e4fabbf192dbbba509e5762cd8018429
Peter de Jong <p.de.jong@gmail.com>
2012-08-01 16:59:25 +0200

    Added shortcut to delete buffer+frame

--- conf-daemon.el
+++ conf-daemon.el
@@ -2,11 +2,18 @@
 (server-start)
 ;; open file thru server in new frame
 (add-hook 'server-switch-hook
-	  (lambda nil
-	    (let ((server-buf (current-buffer)))
-	      (bury-buffer)
-	      (switch-to-buffer-other-frame server-buf))))
+          (lambda nil
+            (let ((server-buf (current-buffer)))
+              (bury-buffer)
+              (switch-to-buffer-other-frame server-buf))))
 (add-hook 'server-done-hook 'delete-frame)
 (add-hook 'server-done-hook (lambda nil (kill-buffer nil)))
+;; close frame+buffer
+(defun closeFrameAndBuffer ()
+       (interactive)
+       (kill-buffer nil)
+       (delete-frame)
+)
+(global-set-key (kbd "C-x c") 'closeFrameAndBuffer)
 ;; provide
 (provide 'conf-daemon)
